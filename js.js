5dd018ff6b10644fef466314fea99df71904158c
Thales Fiscus <thalesgabriel07@gmail.com>
2024-07-26 08:54:34 -0300

    second commit

--- js.js
+++ js.js
@@ -1,21 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const inputField = document.getElementById('inputField');
-    const output = document.getElementById('output');
+    document.addEventListener('DOMContentLoaded', function() {
+    const input = document.getElementById('input');
 
-    let shouldDisplayNextInput = false; // Flag para controlar o comportamento
+    let nextTitle = false; 
 
-    inputField.addEventListener('keypress', function(event) {
+    input.addEventListener('keypress', function(event) {
         if (event.key === 'Enter') {
-            event.preventDefault(); // Impede o comportamento padrão do Enter no input
+            event.preventDefault(); 
 
-            if (shouldDisplayNextInput) {
-                const input = inputField.value.trim(); // Obtém e limpa o valor do input
-                output.innerHTML = `<strong>${input}</strong>`; // Exibe o texto em negrito
-                inputField.value = ''; // Limpa o campo de entrada
-                shouldDisplayNextInput = false; // Reseta a flag
-            } else if (inputField.value.trim() === "/1") {
-                inputField.value = ''; // Limpa o campo de entrada
-                shouldDisplayNextInput = true; // Define a flag para mostrar o próximo input em negrito
+            if (nextTitle) {
+                const titleText = input.value.trim(); 
+                title.innerHTML = `<h1>${titleText}</h1>`; 
+                input.value = ''; 
+                nextTitle = false; 
+                input.placeholder = 'Type / for blocks, @ to link docs or people';
+                input.classList.remove('bold-placeholder');
+            } else if (input.value.trim() === "/1") {
+                input.value = ''; 
+                nextTitle = true; 
+                input.placeholder = 'Heading 1';
+                input.classList.add('bold-placeholder');
+            } else{
+                text.innerHTML = input.value;
+                input.value = ''; 
             }
         }
     });
